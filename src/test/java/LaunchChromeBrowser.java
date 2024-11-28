285139c3271184bea9a6263c292d787be25bb476
LavanyaPrabhakar <Lavanya.Prabhakar@gmail.com>
2024-11-28 10:28:36 +0000

    First commit is LaunchChromeBrowser

---
+++ src/test/java/LaunchChromeBrowser.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChromeBrowser {
+
+	public static void main(String[] args) {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://letcode.in/test");
+	driver.manage().window().maximize();
+	String title = driver.getTitle();
+	System.out.println("Title of the webpage = "+title);
+	String currentLink =driver.getCurrentUrl();
+	System.out.println("url of the website = "+currentLink);
+	driver.close();
+
+	}
+
+}
